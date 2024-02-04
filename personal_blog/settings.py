da94f747647ea66bfc7f62003b6113a8b7063a95
ferhad <ferhadmislim@gmail.com>
2024-02-03 22:55:53 -0500

    Update database configuration to use PostgreSQL

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -76,24 +76,16 @@ WSGI_APPLICATION = 'personal_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# SQLite3
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': 'blog',
+        'HOST': 'db',    
+        'PORT': '5432',        
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'blog',
-#         'USER': 'blog',
-#         'PASSWORD': 'blog',
-#         'HOST': 'db',    
-#         'PORT': '5432',        
-#     }
-# }
 
 
 
